b2fcef51bb91c2c60f48b6da39b326da99f7a92f
Aleeza Saleem <150428514+aleezasaleem@users.noreply.github.com>
2024-02-26 21:16:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//Question#1:Install Node.js, TypeScript and VS Code on your computer
+//Question2:ersonal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var Person = "Ali";
+console.log("Hello ".concat(Person, " would you like to learn python today?"));
+//Question#3:Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var FirstName = "Eric";
+console.log(FirstName.toLowerCase());
---
+++ main.ts
@@ -0,0 +1,9 @@
+//Question#1:Install Node.js, TypeScript and VS Code on your computer
+
+//Question2:ersonal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let Person:string="Ali"
+console.log(`Hello ${Person} would you like to learn python today?`);
+//Question#3:Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let FirstName:string="Eric"
+console.log(FirstName.toLowerCase());
+
